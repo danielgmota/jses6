00a4287c126b8a6eef1988a56a14c6abc21a8e91
danielgmota <guimaraes15@gmail.com>
2019-11-04 16:18:06 -0300

    functions imports/exports

---
+++ examples/exports_imports.js
@@ -0,0 +1,8 @@
+import soma, { sub as subtracao } from './my_functions';
+
+import * as funcoes from './funcoes';
+
+console.log(soma(1,2));
+console.log(subtracao(6,3));
+
+console.log(funcoes.multi(1,3));
